630c1713b3e21539b5c57f7eb2b17ff75a931636
Corey Baines <coreybaines@spiritdevs.com>
2017-05-20 22:17:49 +1000

    Minor improvements
    New waiting time and callback working

--- src/main/java/com/twitServer/service/TwitCrawl.java
+++ src/main/java/com/twitServer/service/TwitCrawl.java
@@ -2,12 +2,15 @@ package com.twitServer.service;
 
 import com.twitServer.async.Callback;
 import com.twitServer.async.config.SpringAsyncConfig;
+import org.apache.commons.lang.time.DateUtils;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Date;
 import java.util.concurrent.ExecutionException;
 
 /**
@@ -15,45 +18,57 @@ import java.util.concurrent.ExecutionException;
  */
 public class TwitCrawl {
 
+    static final long DAY = 24 * 60 * 60 * 1000;
+    static final long APIWait = 15 * 1000;
+
+    public boolean apiWait24Hours(Date aDate) {
+        return aDate.getTime() > System.currentTimeMillis() - DAY;
+    }
+
+    public boolean validAPIWait(Date aDate) {
+        System.out.println(aDate.getTime());
+        System.out.println(System.currentTimeMillis());
+        System.out.println(APIWait);
+        return aDate.getTime() > System.currentTimeMillis() - APIWait;
+    }
 
 
-    //    @Override
-//    public void twitAPICallback(String key, JSONArray code) {
-//        System.out.println("HELLO FROM INSIDE THE CALLBACK");
-//
-//        ShowsMongo showsMongo = new ShowsMongo();
-//        showsMongo.updateShows(key, code);
-//    }
+    public void startServer() {
+
+        //Scan mongoDB and see if there has been any changes
+        validAPIWait(new Date());
 
 
-    public void startServer() throws IOException, JSONException, InterruptedException, ExecutionException {
+
+    }
+    public void startTwitAPICrawl(String baseURL, int showID, int pageNumber) throws IOException, JSONException, InterruptedException, ExecutionException {
+
+        String coreURL = "https://twit.tv/api/v1.0/" + baseURL;
+
+        if (baseURL == "shows") {
+            if (showID > 0) {
+                coreURL += "/" + showID;
+            }
+        } else {
+            if (pageNumber > 0) {
+                coreURL += "?page=" + pageNumber;
+            }
+            if (pageNumber > 0 && showID > 0) {
+                coreURL += "&filter%5Bshows%5D=" + showID;
+            }
+        }
 
         ApplicationContext context = new AnnotationConfigApplicationContext(SpringAsyncConfig.class);
         TwitAPI twitAPI = context.getBean(TwitAPI.class);
-        System.out.println("THIS IS BEFORE");
-        twitAPI.readFromTwitAPI("https://twit.tv/api/v1.0/Shows", new Callback() {
+
+        twitAPI.readFromTwitAPI(coreURL, new Callback() {
             @Override
             public void twitAPICallback(String key, JSONArray code) {
-                System.out.println(key);
+                ShowsMongo showsMongo = new ShowsMongo();
+                showsMongo.updateShows(key, code);
             }
         });
-
-        System.out.println("THIS IS JUST AFTER");
-//            if (twitCode.isDone()) {
-//                if (twitCode.get().getJSONArray("shows") != null) {
-//                    callback.twitAPICallback("shows", twitCode.get().getJSONArray("shows")); //updateShows("shows", twitCode.get().getJSONArray("shows"));
-//                }
-//            }
-//
-//        System.out.println("THIS IS AFTER THE WHILE");
     }
-//    public void startTwitAPICrawl(String baseURL, int showID, int pageNumber) throws IOException, JSONException, InterruptedException, ExecutionException {
-//
-//        ApplicationContext context = new AnnotationConfigApplicationContext(SpringAsyncConfig.class);
-//        TwitAPI twitAPI = context.getBean(TwitAPI.class);
-//
-//        twitAPI.readFromTwitAPI();
-//    }
 
 
 
